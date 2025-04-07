5aa5f467ad93e6a98f48c50382281927f152af38
vitormcruz <vitormcruz@gmail.com>
2025-04-07 14:43:29 -0300

    Initial application with spring boot and minimal configuration for Auth0 jwt key.

---
+++ src/main/java/br/com/vmc/auction/AuctionCaseStudyApplication.java
@@ -0,0 +1,13 @@
+package br.com.vmc.auction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuctionCaseStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuctionCaseStudyApplication.class, args);
+	}
+
+}
