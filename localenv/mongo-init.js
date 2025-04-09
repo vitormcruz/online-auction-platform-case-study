6ed96859a82e19fd27b55a376f4b31ab52677aaa
vitormcruz <vitormcruz@gmail.com>
2025-04-09 17:18:01 -0300

    Creating frontend (angular) and moving java to backend.

--- localenv/mongo-init.js
+++
@@ -1,8 +0,0 @@
-/**
- * This is a bullshit file needed by the MondgoDB because it's container creation do not create the database even if
- * I explicitly set MONGO_INITDB_DATABASE environment variable, I must also provide something to put inside it because,
- * for some reason, they dislike empty databases.
- */
-
-use("mydatabase");
-db.createCollection("dummy");
