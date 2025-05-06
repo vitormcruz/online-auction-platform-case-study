50497cd23f5fa71459d67226347b617538a48b0e
vitormcruz <vitormcruz@gmail.com>
2025-05-06 11:06:56 -0300

    React application with MaterialUI

---
+++ frontend/app/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import {Button} from "@mui/material";
+
+export default function Home() {
+  return (
+      <div>
+        <h1><b>Auction Plataform</b></h1>
+
+        <Button variant="contained">Hello world</Button>
+      </div>
+    );
+}
