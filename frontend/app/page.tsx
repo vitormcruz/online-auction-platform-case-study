4e2160c06c157a96f557f05626cc6478bfdc030c
vitormcruz <vitormcruz@gmail.com>
2025-05-09 14:50:31 -0300

    Adding an AppBar

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,12 +1,29 @@
-import Image from "next/image";
-import {Button} from "@mui/material";
+import {AppBar, Box, Button, IconButton, Toolbar, Typography} from "@mui/material";
+import MenuIcon from '@mui/icons-material/Menu';
 
 export default function Home() {
   return (
       <div>
-        <h1><b>Auction Plataform</b></h1>
+          <Box sx={{ flexGrow: 1 }}>
+              <AppBar position="static">
+                  <Toolbar>
+                      <IconButton
+                          size="large"
+                          edge="start"
+                          color="inherit"
+                          aria-label="menu"
+                          sx={{ mr: 2 }}
+                      >
+                          <MenuIcon />
+                      </IconButton>
+                      <Typography variant="h4" component="div" sx={{ flexGrow: 1 }}>
+                          Auction Plataform
+                      </Typography>
+                      <Button color="inherit">Login</Button>
+                  </Toolbar>
+              </AppBar>
+          </Box>
 
-        <Button variant="contained">Hello world</Button>
       </div>
     );
 }
